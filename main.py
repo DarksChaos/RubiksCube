5c80d43b016fe5759d226deaf8c6d2057d2c7da9
DarksChaos <nfb5901@gmail.com>
2022-09-12 19:08:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,11 @@ import tkinter as tk
 
 moves = [
     [0, 2, 8, 6, 1, 5, 7, 3, 9, 18, 36, 45, 10, 19, 37, 46, 11, 20, 38, 47],  # U
-    [9, 11, 17, 15, 10, 14, 16, 12, 2, 51, 29,
-        20, 5, 48, 32, 23, 8, 45, 35, 26],  # R
-    [18, 20, 26, 24, 19, 23, 25, 21, 6, 9, 29,
-        44, 7, 12, 28, 41, 8, 15, 27, 38],  # F
-    [27, 29, 35, 33, 28, 32, 34, 30, 24, 15, 51,
-        42, 25, 16, 52, 43, 26, 17, 53, 44],  # D
-    [36, 38, 44, 42, 37, 41, 43, 39, 0, 18, 27,
-        53, 3, 21, 30, 50, 6, 24, 33, 47],  # L
-    [45, 47, 53, 51, 46, 50, 52, 48, 2, 36, 33,
-        17, 1, 39, 34, 14, 0, 42, 35, 11]  # B
+    [9, 11, 17, 15, 10, 14, 16, 12, 2, 51, 29,20, 5, 48, 32, 23, 8, 45, 35, 26],  # R
+    [18, 20, 26, 24, 19, 23, 25, 21, 6, 9, 29, 44, 7, 12, 28, 41, 8, 15, 27, 38],  # F
+    [27, 29, 35, 33, 28, 32, 34, 30, 24, 15, 51, 42, 25, 16, 52, 43, 26, 17, 53, 44],  # D
+    [36, 38, 44, 42, 37, 41, 43, 39, 0, 18, 27, 53, 3, 21, 30, 50, 6, 24, 33, 47],  # L
+    [45, 47, 53, 51, 46, 50, 52, 48, 2, 36, 33, 17, 1, 39, 34, 14, 0, 42, 35, 11]  # B
 ]
 
 ColorScheme = [
@@ -52,15 +47,16 @@ Stickers = [  # sticker size: 50x50
 ]
 
 Widgets = {
-    "RESET": [50, 500],
+    "RESET": [55, 500],
     "MANEUVER": [100, 500],
     "APPLY": [410, 500],
-    "FIND": [50, 530],
+    "FIND": [55, 528],
     "SOLUTION": [100, 530],
     "PREVIOUS": [480, 500],
     "CURRENT": [515, 490],
     "NEXT": [580, 500],
-    "EMPTY": [50, 560]
+    "EMPTY": [4, 500],
+    "HINT": [4, 528]
 }
 
 FacesIndex = "URFDLB"
@@ -78,6 +74,10 @@ SelectedColor = 0
 PatternSize = 50
 
 PatternOffsetX = PatternOffsetY = 0
+
+ShowFaceHints = False
+
+Hints = []
 
 
 def StringToIndex(face):
@@ -169,13 +169,14 @@ def GetColorCenterFace(center):
 
 
 def draw_stickers(offx=5, offy=5, gray=False):
-    global PatternSize, PatternOffsetX, PatternOffsetY
+    global PatternSize, PatternOffsetX, PatternOffsetY, ShowFaceHints, Hints
     PatternOffsetX = offx
     PatternOffsetY = offy
 
     ps = PatternSize
 
     def draw_sticker(pos):
+        
         x, y = Stickers[pos]
         x *= ps
         y *= ps
@@ -185,14 +186,29 @@ def draw_stickers(offx=5, offy=5, gray=False):
             color = "#888888"
         else:
             color = ColorScheme[State[pos]]
-        Canvas.create_rectangle(
-            x, y, x+ps, y+ps, fill=color, outline="#000000", width="3")
+        Canvas.create_rectangle(x, y, x+ps, y+ps, fill=color, outline="#000000", width="3")
         Canvas.pack()
 
     i = 0
     while i < 54:
         draw_sticker(i)
         i += 1
+
+    if ShowFaceHints:
+        i = 0
+        Hints = []
+        while i < 6:
+            hint = tk.Label(Root, text=FacesIndex[i], bg=ColorScheme[State[Core[i]]], font=("Roboto", 25))
+            x, y = Stickers[Core[i]]
+            x *= PatternSize
+            y *= PatternSize
+            hint.place(x=x+16, y=y+10)
+            Hints.append(hint)
+            i += 1
+    else:
+        for i in Hints:
+            i.destroy()
+        Hints = []
 
 
 def ParseMove(move):
@@ -345,6 +361,14 @@ def draw_widgets():
     empty = tk.Button(Root, text="EMPTY", command=Empty)
     empty.place(x=Widgets["EMPTY"][0], y=Widgets["EMPTY"][1])
 
+    def ShowFaceHint():
+        global ShowFaceHints
+        ShowFaceHints = not ShowFaceHints
+        draw_stickers()
+    WidgetFunctions["ShowFaceHint"] = ShowFaceHint
+
+    hint = tk.Button(Root, text="HINT", command=ShowFaceHint)
+    hint.place(x=Widgets["HINT"][0], y=Widgets["HINT"][1])
 
 def OnMouseClick(event):
     global PatternSize, PatternOffsetX, PatternOffsetY, WidgetFunctions, SelectedColor
